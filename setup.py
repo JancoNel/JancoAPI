6dc4f0ceffe1b4678d93f8bebdcb233e32eefd1f
Janco Nel <neljanco92@Gmail.com>
2024-09-05 19:02:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="JancoAPI",
     version="0.1.0",
     description="A Python library with random and niche functions",
-    long_description=long_description,
-    long_description_content_type="text/markdown",  # Explicitly set to Markdown
+    long_description="This is a Python package that offers various utilities.",  # Hardcoded vague description
+    long_description_content_type="text/plain",  # Set content type to plain text
     author="Janco Nel",
-    author_email="your_email@example.com",
     url="https://github.com/JancoNel/JancoAPI",
     packages=find_packages(),
     classifiers=[
